e7032b420c8b373be673b6ed61223444f03e482e
conrad bezuidenhout <conradbez@conrads-MacBook-Air.local>
2025-07-14 16:31:40 +0800

    feat: init

---
+++ Brewfile
@@ -0,0 +1,9 @@
+cask "brave-browser"
+cask "visual-studio-code"
+
+mas "Magnet", id: 441258766  
+
+brew "gh"
+brew "uv"
+brew "node"
+brew "fish"
---
+++ install.sh
@@ -0,0 +1,7 @@
+brew bundle --file=./Brewfile
+
+gh auth login
+
+npm install -g @anthropic-ai/claude-code
+
+defaults write com.apple.dock autohide -bool true && killall Dock
